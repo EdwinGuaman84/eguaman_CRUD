44673bbba1ab934bc88a3eb67edb5c004f0d634d
EdwinGuaman <ed-guaman@hotmail.com>
2025-05-16 03:20:12 -0500

    Add project files.

---
+++ Data/PersonRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eguaman_CRUD.Modelos;
+using SQLite;
+
+namespace eguaman_CRUD.Data
+{
+    public class PersonRepository
+    {
+        string _dbPath;
+        private SQLiteConnection conn;
+        public string StatusMessage { get; set;  }
+
+        private void Init()
+        {
+            if (conn is not null)
+                return;
+            conn = new(_dbPath);
+            conn.CreateTable<Persona>();
+         }
+        public PersonRepository(string bdPath) { 
+
+            _dbPath = bdPath;
+        }
+
+    }
+}
---
+++ Modelos/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace eguaman_CRUD.Modelos
+{
+    [Table("persona")]
+    public class Persona
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(25), Unique]
+        public string Name { get; set; }
+
+        
+
+    }
+}
